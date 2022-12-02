37bbb026762f6c00020a59bd7724ed71cf147c91
Oxygen-182 <119591922+Oxygen-182@users.noreply.github.com>
2022-12-02 18:39:25 +0300

    Add files via upload

---
+++ ConsoleApp_3_2/ConsoleApp_3_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_3_2
+{
+    class Program
+    {
+        static double f(double x)
+        {
+            double y;
+            if (x < 3) y = x * x - 0.3;
+            else if (x > 5) y = x * x + 1;
+            else y = 0;
+            return y;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b=");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("h=");
+            double h = double.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("Параметр a не может быть больше параметра b");
+                return;
+            }
+            if (h < 0)
+            {
+                Console.WriteLine("Параметр h не может быть отрицательным");
+                return;
+            }
+
+            for (double i = a; i <= b; i += h)
+                Console.WriteLine("f({0:f2})={1:f2}", i, f(i));
+        }
+    }
+}
---
+++ ConsoleApp_3_2_form/ConsoleApp_3_2_form/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp_3_2_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static double f(double x)
+        {
+            double y;
+            if (x < 3) y = x * x - 0.3;
+            else if (x > 5) y = x * x + 1;
+            else y = 0;
+            return y;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox4.Clear();
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double h = double.Parse(textBox3.Text);
+
+            if(a > b)
+            {
+                textBox4.Text += "Параметр a не может быть больше параметра b";
+                return;
+            }
+            if (h < 0)
+            {
+                textBox4.Text += "Параметр h не может быть отрицательным";
+                return;
+            }
+
+            textBox4.Text += "Значения функции f(x)";
+            textBox4.Text += Environment.NewLine;
+
+            for (double i = a; i <= b; i += h)
+            {
+                textBox4.Text += Environment.NewLine;
+                textBox4.Text += ($"f({i:f2})={f(i):f2}");
+            }
+        }
+    }
+}
